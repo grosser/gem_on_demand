62a78ae24673d54aac0468b366d19d639abb42f0
grosser <michael@grosser.it>
2014-02-27 19:50:54 -0800

    cache

--- lib/gem_on_demand.rb
+++ lib/gem_on_demand.rb
@@ -1,9 +1,12 @@
 require 'tmpdir'
 
 module GemOnDemand
+  CACHE = "cache"
+  CACHE_DURATION = 30 # seconds
+
   class << self
     def build_gem(user, project, version)
-      clone_project(user, project) do
+      inside_of_project(user, project) do
         checkout_version("v#{version}")
         gemspec = "#{project}.gemspec"
         remove_signing(gemspec)
@@ -14,7 +17,7 @@ module GemOnDemand
 
     def dependencies(user, gems)
       gems.map do |project|
-        clone_project(user, project) do
+        inside_of_project(user, project) do
           versions = sh("git tag").split($/).grep(/^v\d+\.\d\.\d$/)
           puts "VERSIONS #{versions}"
           versions.last(2).map do |version|
@@ -36,21 +39,44 @@ module GemOnDemand
     def sh(command)
       puts command
       result = `#{command}`
-      raise unless $?.success?
+      raise "Command failed: #{result}" unless $?.success?
       result
     end
 
-    def clone_project(user, project, &block)
-      Dir.mktmpdir do |dir|
-        Dir.chdir(dir) do
-          sh "git clone https://github.com/#{user}/#{project}.git"
-          Dir.chdir(project, &block)
-        end
+    def inside_of_project(user, project, &block)
+      Dir.mkdir(CACHE) unless File.directory?(CACHE)
+      Dir.chdir(CACHE) do
+        clone_or_refresh_project(user, project)
+        Dir.chdir(project, &block)
       end
     end
 
+    def clone_or_refresh_project(user, project)
+      if File.directory?(project)
+        if refresh?(project)
+          Dir.chdir(project) { sh "git fetch origin" }
+          refreshed!(project)
+        end
+      else
+        sh "git clone https://github.com/#{user}/#{project}.git"
+        refreshed!(project)
+      end
+    end
+
+    def refreshed!(project)
+      File.write(updated(project), Time.now.to_i)
+    end
+
+    def refresh?(project)
+      File.read(updated(project)).to_i < Time.now.to_i - CACHE_DURATION
+    end
+
+    def updated(project)
+      "#{project}.updated_at"
+    end
+
     def checkout_version(version)
-      sh("git checkout #{version}")
+      sh("git checkout #{version} --force")
     end
 
     # ERROR:  While executing gem ... (Gem::Security::Exception)
